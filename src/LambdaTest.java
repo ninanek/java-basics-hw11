0edb3f56e8f695f298b5e7ffa00daa1d2f07ac42
Nina Nekrasova <nina.nekrasova@ring.com>
2020-05-17 20:04:58 +0300

    hw_11

--- src/LambdaTest.java
+++ src/LambdaTest.java
@@ -15,8 +15,10 @@ public class LambdaTest {
                 {-1.0, 10.0, 10.0},
                 {0.0, 0.0, 0.0},
                 {-10.0, NaN, NaN},
-                {POSITIVE_INFINITY, 1e32, POSITIVE_INFINITY}
-                // TODO add 2 more test data here
+                {POSITIVE_INFINITY, 1e32, POSITIVE_INFINITY},
+                {10.0, 10.1, 10.1},
+                {100.0, 0.0, 100.0},
+
         };
     }
 
@@ -25,8 +27,10 @@ public class LambdaTest {
         return new Object[][]{
                 {25.0, 5.0},
                 {-1.0, NaN},
-                {POSITIVE_INFINITY, POSITIVE_INFINITY}
-                // TODO add 2 more test data here
+                {POSITIVE_INFINITY, POSITIVE_INFINITY},
+                {NaN, NaN},
+                {-10.0, NaN}
+
         };
     }
 
